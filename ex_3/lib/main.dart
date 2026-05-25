6ad6b698883f18ce6f5d857a55ea8222910b6536
Loun Rotha <lounrotha4009@gmail.com>
2026-05-25 14:05:29 +0700

    ex3

--- ex_3/lib/main.dart
+++ ex_3/lib/main.dart
@@ -6,19 +6,31 @@ void main() {
       home: Container(
         padding: EdgeInsets.all(5),
         margin: EdgeInsets.all(5),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Center(
-            child: Column(
-              children: [
-                Text('OOP', style: TextStyle(color: Colors.white)),
-                Text('DART', style: TextStyle(color: Colors.white)),
-                Text('FLUTTER', style: TextStyle(color: Colors.white)),
-              ],
-            ),
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue[100],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text('OOP', style: TextStyle(color: Colors.white)),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue[200],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text('DART', style: TextStyle(color: Colors.white)),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue[300],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text('FLUTTER', style: TextStyle(color: Colors.white)),
+              ),
+            ],
           ),
         ),
       ),
