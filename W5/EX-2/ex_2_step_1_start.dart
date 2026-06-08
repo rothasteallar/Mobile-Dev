8ac61d693291b7bde450d85c9d951b4a008f3f9a
Loun Rotha <lounrotha4009@gmail.com>
2026-06-08 21:32:50 +0700

    update code

--- W5/EX-2/ex_2_step_1_start.dart
+++ W5/EX-2/ex_2_step_1_start.dart
@@ -1,18 +1,82 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: const Text("Favorite cards"),
-        ),
-        body: Column(
-          children: [
-            Container(
-                // YOUR CODE
-                )
-          ],
-        ),
+class FavoriteCard extends StatefulWidget {
+  final String title;
+  final String description;
+
+  const FavoriteCard({
+    super.key,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  State<FavoriteCard> createState() => _FavoriteCardState();
+}
+
+class _FavoriteCardState extends State<FavoriteCard> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        border: Border(bottom: BorderSide(width: .5, color: Colors.grey)),
       ),
-    ));
+      padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Expanded(
+            flex: 7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.title,
+                  style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                Text(widget.description, style: TextStyle(color: Colors.black)),
+              ],
+            ),
+          ),
+
+          IconButton(
+            onPressed: () => {
+              setState(() {
+                isFavorite = !isFavorite;
+              }),
+            },
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+              color: isFavorite ? Colors.red : Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text("Favorite cards"),
+      ),
+      body: Column(
+        children: [
+          FavoriteCard(description: "Learn DART With Me", title: "DART"),
+          FavoriteCard(description: "Learn FLUTTER With Me", title: "FLUTTER"),
+          FavoriteCard(description: "Learn OOP With Me", title: "OOP"),
+        ],
+      ),
+    ),
+  ),
+);
