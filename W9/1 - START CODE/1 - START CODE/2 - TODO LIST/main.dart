c90ae092ae6c630f8d6db7e6ae889e166fa36ad6
Loun Rotha <lounrotha4009@gmail.com>
2026-07-05 23:52:15 +0700

    code

--- W9/1 - START CODE/1 - START CODE/2 - TODO LIST/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'ui/screens/todos/todos_screen.dart';
-
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: TodosScreen()));
-}
